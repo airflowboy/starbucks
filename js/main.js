272aff83f2c6f002ae3e58cb2efc11395e026ff4
airflowboy <ajtwoddl9194@gmail.com>
2025-01-09 15:33:59 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-// 돋보기 아이콘을 클릭해도 input에 focus가 되도록
-searchEl.addEventListener('click', function() {
-    searchInputEl.focus(); 
-});
-
-searchInputEl.addEventListener('focus', function() {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function() {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -153,9 +134,4 @@ new Swiper('.awards .swiper-container', {
         prevEl: '.awards .swiper-prev',
         nextEl: '.awards .swiper-next',
     }
-})
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
-
-
+});
